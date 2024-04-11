5a553696b62365ca2da1ffe42641feacd05100c0
NatthanichaP <natthanicha.pa@rmutsvmail.com>
2024-04-11 23:04:52 +0700

    nnnnn
    nnnn
    
    Co-Authored-By: morphine035 <154063495+morphine035@users.noreply.github.com>

--- page.php
+++ page.php
@@ -162,27 +162,19 @@
         <section class="parking-container">
             <div class="parking-lot">
                 <?php
-                
                     if ($result->num_rows > 0) {
-                        // เมื่อพบผู้ใช้ในระบบ
                         $row = $result->fetch_assoc();
                         $current_user_id = $row['userid']; // กำหนดค่าของ userid ให้กับตัวแปร $current_user_id
                     }
                         if(isset($current_user_id)) {
-                            // ตรวจสอบสถานะของผู้ใช้ว่าเป็นสมาชิกหรือไม่
                             $sql_member_check = "SELECT * FROM tbmember WHERE userid = $current_user_id"; // $current_user_id เป็นตัวแปรที่เก็บค่า ID ของผู้ใช้ปัจจุบัน
                             $result_member_check = $conn->query($sql_member_check);
-                        
                             if ($result_member_check->num_rows > 0) {
-                                // ผู้ใช้เป็นสมาชิก สามารถแสดงไอคอนรถเพื่อทำการจองได้
                                 $sql_parkingslots = "SELECT * FROM tbparkingslot";
                                 $result_parkingslots = $conn->query($sql_parkingslots);
-                                
                                 if ($result_parkingslots->num_rows > 0) {
-                                    // แสดงช่องจอดรถแต่ละช่อง
                                     while($row_parkingslots = $result_parkingslots->fetch_assoc()) {
                                         if($row_parkingslots['p_status'] == 0) {
-                                            // ถ้าสถานะเป็น 0 (ว่าง) ให้แสดงเป็นสีเขียว
                                             echo '<div class="parking-slot available" ><a href="booking.php?p_slotid=' . $row_parkingslots['p_slotid'] . '">' . $row_parkingslots['channelnumber'] . '
                                                 <br>
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="green" class="bi bi-car-front-fill" viewBox="0 0 16 16">
@@ -190,14 +182,12 @@
                                                 </svg>
                                             </a> </div>';
                                         } else {
-                                            // ถ้าสถานะเป็น 1 (ถูกจอง) ให้แสดงเป็นสีแดง
                                             echo '<div class="parking-slot occupied "><a href="#" >' . $row_parkingslots['channelnumber'] . '
                                                 <br>
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="red" class="bi bi-car-front-fill" viewBox="0 0 16 16">
                                                     <path d="M2.52 3.515A2.5 2.5 0 0 1 4.82 2h6.362c1 0 1.904.596 2.298 1.515l.792 1.848c.075.175.21.319.38.404.5.25.855.715.965 1.262l.335 1.679q.05.242.049.49v.413c0 .814-.39 1.543-1 1.997V13.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1.338c-1.292.048-2.745.088-4 .088s-2.708-.04-4-.088V13.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1.892c-.61-.454-1-1.183-1-1.997v-.413a2.5 2.5 0 0 1 .049-.49l.335-1.68c.11-.546.465-1.012.964-1.261a.8.8 0 0 0 .381-.404l.792-1.848ZM3 10a1 1 0 1 0 0-2 1 1 0 0 0 0 2m10 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2M6 8a1 1 0 0 0 0 2h4a1 1 0 1 0 0-2zM2.906 5.189a.51.51 0 0 0 .497.731c.91-.073 3.35-.17 4.597-.17s3.688.097 4.597.17a.51.51 0 0 0 .497-.731l-.956-1.913A.5.5 0 0 0 11.691 3H4.309a.5.5 0 0 0-.447.276L2.906 5.19Z"/>
                                                 </svg>
                                             </a> </div>';
-                                            
                                         }
                                     }
                                 } else {
@@ -205,14 +195,10 @@
                                 }
                             }
                         } else {
-                                // ผู้ใช้ไม่เป็นสมาชิก แสดงข้อความแจ้งให้สมัครสมาชิกก่อนทำการจอง
                                 echo '<script>alert("คุณต้องสมัครสมาชิกก่อนทำการจองได้")</script>';
-                                // แสดงที่ไอคอนจอดรถแต่ไม่สามารถกดได้
                                 $sql_parkingslots = "SELECT * FROM tbparkingslot";
                                 $result_parkingslots = $conn->query($sql_parkingslots);
-                                    
                                 if ($result_parkingslots->num_rows > 0) {
-                                    // แสดงช่องจอดรถแต่ละช่อง
                                     while($row_parkingslots = $result_parkingslots->fetch_assoc()) {
                                         echo '<div class="parking-slot available"><span>' . $row_parkingslots['channelnumber'] . '</span>
                                             <br>
@@ -225,8 +211,6 @@
                                     echo '<script>alert("ไม่พบข้อมูลช่องจอดรถ")</script>';
                                 }
                             }
-
-
                     mysqli_data_seek($result, 0);        
                     ?>
             </div>
